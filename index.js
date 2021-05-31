c419a719e2b0ea733ce58d28a620fd3833c450ee
chaoyuquenet <chaoyu1979@gmail.com>
2021-05-31 15:43:29 +0200

    changed index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ Hi: 'there' })
+  res.send({ node: 'first app' })
 });
 
 const PORT = process.env.PORT || 5000;
