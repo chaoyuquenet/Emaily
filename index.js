26a143d3ffa80b8dd4d20188d0507175384491be
chaoyuquenet <chaoyu1979@gmail.com>
2021-06-17 11:32:01 +0200

    finished auth flow

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+const authRoutes = require('./routes/authRoutes');
+require('./models/User');
+require('./services/passport');
+
+mongoose.connect(keys.mongodbURI);
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ node: 'first app' })
-});
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+)
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+authRoutes(app);
 
 const PORT = process.env.PORT || 5000;
 
